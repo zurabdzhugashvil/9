bafdb5ccd42918af3d18353368e0a1b2164ace31
zurabdzhugashvil <zurabdzhugashvili@gmail.com>
2025-07-29 01:22:36 +0200

    Add files via upload

---
+++ difference.js
@@ -0,0 +1,17 @@
+// perform difference operation
+// elements of set a that are not in set b
+function difference(setA, setB) {
+    let differenceSet = new Set(setA)
+    for (let i of setB) {
+        differenceSet.delete(i)
+    }
+    return differenceSet
+}
+
+// two sets of fruits
+let setA = new Set(['apple', 'mango', 'orange']);
+let setB = new Set(['grapes', 'apple', 'banana']);
+
+let result = difference(setA, setB);
+
+console.log(result);
