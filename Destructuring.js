24eeec44dbed5295a28e2df158c9d4d8631ef5d1
zurabdzhugashvil <zurabdzhugashvili@gmail.com>
2025-08-26 04:26:38 +0200

    Add files via upload

---
+++ Destructuring.js
@@ -0,0 +1,14 @@
+// assigning object attributes to variables
+const person = {
+    name: 'Sara',
+    age: 25,
+    gender: 'female'    
+}
+
+let name = person.name;
+let age = person.age;
+let gender = person.gender;
+
+console.log(name); // Sara
+console.log(age); // 25
+console.log(gender); // female
